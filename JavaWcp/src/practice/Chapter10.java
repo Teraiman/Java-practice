0107e22e0f76260f3a86381676e4b1b7d12e6d63
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-11-04 18:06:05 +0900

    [save]基礎編第10章 確認問題

--- JavaWcp/src/practice/Chapter10.java
+++ JavaWcp/src/practice/Chapter10.java
@@ -2,4 +2,17 @@ package practice;
 
 public class Chapter10 {
 
+	public static void main(String[] args) {
+		
+		try {
+			int array[] = {1, 3, 5};
+			Chapter10.validIndex(array, 2);
+			Chapter10.validIndex(array, 3);
+			
+			System.out.println("validIndecの呼び出し終了");
+		} catch (IllegalArgumentException e) {
+			System.out.println("IllegalArgumentExceptionが発生しました");
+			e.printStackTrace();
+		}
+	}
 }
