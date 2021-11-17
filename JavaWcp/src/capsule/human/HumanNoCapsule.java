aa4d07103bc7cfe725ed9fd583cf2a6817c349ef
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-11-17 10:44:55 +0900

    [finish]基礎編第12章 カプセル化(HumanNoCapsule)

--- JavaWcp/src/capsule/human/HumanNoCapsule.java
+++ JavaWcp/src/capsule/human/HumanNoCapsule.java
@@ -2,4 +2,11 @@ package capsule.human;
 
 public class HumanNoCapsule {
 
+	public String name;
+	public int age;
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = agae;
+	}
 }
